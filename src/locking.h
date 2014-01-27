62d7f975a6f44bee474c85107ceca2731fc06ec1
Mark Grennan <mark@grennan.com>
2014-01-27 20:35:35 +0000

    New dht22 program replacing temp

---
+++ src/locking.h
@@ -0,0 +1,10 @@
+/*
+ *      locking.c:
+ *      Prevents rapid use of application by implementing a file lock
+ *      technion@lolware.net
+ */
+
+#define LOCKFILE "/var/run/dht.lock"
+
+int open_lockfile(const char *filename);
+void close_lockfile(int fd);
