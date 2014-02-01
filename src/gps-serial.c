a8bfafd67e319362ca1c5f679910c7769a5edb3a
Mark Grennan <mark@grennan.com>
2014-02-01 03:35:51 +0000

    Added command line options

--- src/gps-serial.c
+++ src/gps-serial.c
@@ -1,36 +1,69 @@
 /*
-//  position.c
+//  gps-serial
 //  Mark Grennan - 2014-01-17
-//
-//  Using the MCP3008 AD converter (ADC) read the Air Quality
-//  insterment and print the values.
-//
-//  based on isr.c from the WiringPi library, authored by Gordon Henderson
-//  https://github.com/WiringPi/WiringPi/blob/master/examples/isr.c
-//
-//  Compile as follows:
-//
-//      gcc -o position position.c -lwiringPi
-//
-//  Run as follows:
-//
-//      sudo ./position
 */
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdlib.h>
+#include <getopt.h>
+#include <math.h>
 #include "gps.h"
 
-int main(void) {
+void print_usage(const char *);
+void parse_opts(int argc, char *argv[]);
 
-    printf("Display the GPS information.\n");
-    gps_init();                 // Open serial port
+int main (int argc, char *argv[])
+{
 
-    loc_t data;                 // Create structure
+	parse_opts(argc,argv);
 
-    gps_location(&data);        // Read and parse the GPS info
+	gps_init();					 // Open serial port
 
-    printf("Date: 20%02d-%02d-%02d %02d:%02d:%02d Lat: %lf lon: %lf Atl: %lf\n", 
-        data.year, data.month, data.day, data.hour, data.minute, data.second, data.latitude, data.longitude, data.altitude);
+	loc_t data;					 // Create structure
 
-    return EXIT_SUCCESS;
+	gps_location(&data);		 // Read and parse the GPS info
+
+	printf("       Date: 20%02d-%02d-%02d %02d:%02d:%02d\n", data.year, data.month, data.day, data.hour, data.minute, data.second);
+	printf("  Longatude: % 11.4f\n", data.latitude);
+	printf("   Latitude: % 11.4f\n", data.longitude);
+	printf("   Altitude: % 11.4f\n", data.altitude);
+
+	return EXIT_SUCCESS;
+}
+
+void print_usage(const char *prog)
+{
+	puts("\ngps-serial - read GPS data from a serial port\n");
+	printf("Usage: %s [-h]\n", prog);
+	puts("  -h --help\t print this help message");
+	exit(1);
+}
+
+void parse_opts(int argc, char *argv[])
+{
+	while (1)
+	{
+		static const struct option lopts[] =
+		{
+			{ "help", no_argument, NULL, 'h' },
+			{ NULL, 0, 0, 0 },
+		};
+		int c;
+
+		c = getopt_long(argc, argv, "h", lopts, NULL);
+
+		if (c == -1)
+			break;
+
+		switch (c)
+		{
+			break;
+			case 'h':
+				print_usage(argv[0]);
+				break;
+			default:
+				print_usage(argv[0]);
+				break;
+		}
+	}
 }
